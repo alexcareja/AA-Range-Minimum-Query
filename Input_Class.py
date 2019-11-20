58e7713c61139e6f21e42614b85a00919bd6abf7
Alex Careja <44427360+alexcareja@users.noreply.github.com>
2019-11-20 23:02:44 +0200

    Add files via upload

---
+++ Input_Class.py
@@ -0,0 +1,31 @@
+
+class Input():
+
+	def __init__(self):
+		self.vector = []
+		self.indexes = []
+		self.N = 0
+		self.M = 0
+
+	def getVector(self):
+		return self.vector
+
+	def getIndexes(self):
+		return self.indexes
+
+	def readData(self, in_file):
+		data = []
+		with open(in_file, "r") as file:
+			for line in file:
+				line_int = list(map(int, line.split()))
+				for num in line_int:
+					data.append(num)
+		data.reverse()
+		self.N = data.pop()
+		self.M = data.pop()
+		for i in range(self.N):
+			self.vector.append(data.pop())
+		for i in range(self.M):
+			(x, y) = (data.pop(), data.pop())
+			self.indexes.append((x, y))
+			
